52ec8a77706e8df67ca90d4de530c36aa9232405
M-Bahy <mohamedayman2942002@gmail.com>
2025-07-04 21:06:19 +0300

    feat: add ShippableExpirableProduct class extending ExpirableProduct and implementing Shippable interface

---
+++ ShippableExpirableProduct.java
@@ -0,0 +1,24 @@
+import java.time.LocalDate;
+
+public class ShippableExpirableProduct extends ExpirableProduct implements Shippable {
+    private double weight;
+
+    public ShippableExpirableProduct(String name, double price, int quantity, LocalDate expiryDate, double weight) {
+        super(name, price, quantity, expiryDate);
+        setWeight(weight);
+    }
+
+    @Override
+    public void setWeight(double weight) {
+        if (weight <= 0) {
+            throw new IllegalArgumentException("Weight must be a positive value, got: " + weight);
+        }
+        this.weight = weight;
+    }
+
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+
+}
