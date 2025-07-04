5f335bffe1505e0846637694b7c8be40d89ea003
M-Bahy <mohamedayman2942002@gmail.com>
2025-07-04 21:01:49 +0300

    feat: add ShippableProduct class implementing Shippable interface

---
+++ ShippableProduct.java
@@ -0,0 +1,20 @@
+public class ShippableProduct extends Product implements Shippable {
+    private double weight;
+
+    public ShippableProduct(String name, double price, int quantity, double weight) {
+        super(name, price, quantity);
+        setWeight(weight);
+    }
+
+    @Override
+    public void setWeight(double weight) {
+        if (weight <= 0)
+            throw new IllegalArgumentException("Weight must be positive, got: " + weight);
+        this.weight = weight;
+    }
+
+    @Override
+    public double getWeight() {
+        return weight;
+    }
+}
